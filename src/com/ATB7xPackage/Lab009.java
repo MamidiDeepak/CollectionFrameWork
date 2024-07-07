f6036c87500d093c4439d268d5a3dcfbc6ab2394
Deepak M <mamedideepak@gmail.com>
2024-07-07 19:29:26 +0530

    Added swapping of number with variables

---
+++ src/com/ATB7xPackage/Lab009.java
@@ -0,0 +1,24 @@
+package com.ATB7xPackage;
+
+public class Lab009 {
+
+    int a = 2;
+    int b = 3;
+    int c;
+
+    public void swapNumber(){
+
+        c = b;
+        b = a;
+
+    }
+
+    public static void main(String[] args) {
+
+        Lab009 c = new Lab009();
+        c.swapNumber();
+
+        System.out.println("c value is : "+c.c);
+        System.out.println("b value is : "+c.b);
+    }
+}
