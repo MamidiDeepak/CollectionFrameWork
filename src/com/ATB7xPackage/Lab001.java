e96706bfd1b53645709820cc80e0fdc3176be73e
Deepak M <mamedideepak@gmail.com>
2024-07-07 15:13:09 +0530

    new changes

---
+++ src/com/ATB7xPackage/Lab001.java
@@ -0,0 +1,6 @@
+package com.ATB7xPackage;
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/com/ATB7xPackage/multiplicationClass.java
@@ -0,0 +1,17 @@
+package com.ATB7xPackage;
+
+public class multiplicationClass {
+    public static void main(String[] args) {
+//        System.out.println();
+
+        int a = 5;
+
+        //multiplication
+
+        for(int i=1; i<=10; i++){
+
+            System.out.println(a+" * "+i+" = "+a*i);
+        }
+    }
+
+}
