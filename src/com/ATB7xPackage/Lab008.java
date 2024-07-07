6f35ee7d23930b5d40f3c4fdd9cff3c6b92e6db3
Deepak M <mamedideepak@gmail.com>
2024-07-07 19:16:46 +0530

    2 more new class

---
+++ src/com/ATB7xPackage/Lab008.java
@@ -0,0 +1,18 @@
+package com.ATB7xPackage;
+
+public class Lab008 {
+
+        int a = 10;
+        int b = 20;
+
+
+    public static void main(String[] args) {
+
+        Lab008 cal = new Lab008();
+
+        int c = cal.a;
+        int d = cal.b;
+
+        System.out.println(c+d);
+    }
+}
---
+++ src/com/ATB7xPackage/Lan007.java
@@ -0,0 +1,19 @@
+package com.ATB7xPackage;
+
+public class Lan007 {
+    public static void main(String[] args) {
+        System.out.println("With one more method");
+    }
+
+    void player(){
+        String name = "Ishan";
+        String name1 = "Kishan";;
+    }
+
+    public  void bottles(){
+
+        String name = "milton";
+        String name1 = "Borosil";
+
+    }
+}
